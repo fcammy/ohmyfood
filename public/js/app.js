863c8241b1059fa4d2a24649c94e22321900f657
fcammy <facinet.camara@gmail.com>
2021-10-14 23:27:43 +0100

    bastille animation

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,37 @@
-var loader = document.getElementById('loader');
+if(document.querySelector('body').classList.contains('home')) {
+  var loader = document.getElementById('loader');
 
-setTimeout(function()  {
-  loader.style.display = 'none';
-  document.body.style.overflow = 'auto'
-}, 3000);
+  setTimeout(function()  {
+    loader.style.display = 'none';
+    document.body.style.overflow = 'auto'
+  }, 3000);
+
+  document.querySelector('nav button').addEventListener('click', function() {
+    this.classList.toggle('open')
+  })
+}
 
 
-document.querySelector('nav button').addEventListener('click', function() {
-  this.classList.toggle('open')
-})
+
+
+document.querySelectorAll('.card__heart, .menu__heart').forEach(btn => {
+  btn.addEventListener('click', function() {
+    this.classList.toggle('active');
+
+    if(this.classList.contains('active')) {
+      this.children[0].classList.remove('far');
+      this.children[0].classList.add('fas');
+    }else{
+      this.children[0].classList.remove('fas');
+      this.children[0].classList.add('far');
+    }
+  })
+});
+
+document.querySelectorAll('.menu__list .item').forEach(item => {
+  item.addEventListener('click', function() {
+    this.classList.toggle('active');
+  })
+});
+
+
